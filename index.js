3e477cedd8210272c1baa3c4dcbe7c83c31eb3fa
Nitin-Saini24 <nitinsaini5597@gmail.com>
2024-05-09 12:42:25 +0530

    open with modal

--- index.js
+++ index.js
@@ -4,68 +4,36 @@
 const dropable = document.querySelectorAll(".card");
 const backlog = document.querySelector(".backlog");
 const draggable = document.querySelectorAll(".task");
-const add = document.querySelector(".add");
-const modal = document.querySelector(".modal");
-const closeModal = document.querySelector(".closeModal");
-const submit = document.querySelector(".sub");
+const submit = document.querySelector(".sub-1");
 const edit_task = document.querySelector(".edit_task");
+const sub_2 = document.querySelector(".sub-2");
 const delete_task = document.querySelector(".delete_task");
 const backlog_head = document.querySelector(".backlog_head");
 const backlog_content = document.querySelector(".backlog_content");
-const task = document.querySelector(".task");
+const task1 = document.querySelector(".task");
 
-console.log(dropable);
-// const nitin = [];
+// console.log(dropable);
+
+let total_task_count = 0;
+let current_task_count = backlog.children.length + 1;
 
 let newTask;
 
-const openModal = function () {
-  console.log("nitin");
-  modal.classList.remove("active");
-};
-
-const close = function () {
-  modal.classList.add("active");
-};
-
-// closeModal.addEventListener("click", close);
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("active")) {
-    close();
-  }
-});
-
-// draggable.forEach((content) => {
-//   content.addEventListener("dragstart", dragStart);
-// });
-// };
-
 draggable.forEach((task) => {
-  // console.log(task);
   task.addEventListener("dragstart", dragStart);
-  // task.addEventListener("dragend", dragEnd);
 });
-
-add.addEventListener("click", openModal);
 
 dropable.forEach((section) => {
-  console.log(section);
+  // console.log(section);
   section.addEventListener("dragover", dragOver);
   section.addEventListener("drop", dragDrop);
 });
 
 function dragStart() {
   newTask = this;
-  // console.log(newTask);
 }
-// function dragEnd() {
-//   // newTask = null;
-//   // console.log(newTask);
-// }
 
 function dragOver(e) {
-  // console.log("nitin");
   e.preventDefault();
 }
 
@@ -74,18 +42,11 @@ function dragDrop(e) {
   if (e.target.classList.contains("card-body")) {
     e.target.appendChild(newTask);
   }
-  // addButton();
 }
 
-edit_task.addEventListener("click", function () {
-  const newHead = prompt(
-    "Enter new task heading:",
-    backlog_head.querySelector("span").textContent.trim()
-  );
-  const newAbout = prompt(
-    "Enter new task details:",
-    backlog_content.querySelector("span").textContent.trim()
-  );
+function forAddTask() {
+  const newHead = document.querySelector(".my-backlog").value;
+  const newAbout = document.querySelector(".my-about").value;
 
   // Update the task content if the user provided new values
   if (newHead !== null && newAbout !== null) {
@@ -93,33 +54,38 @@ edit_task.addEventListener("click", function () {
     backlog_head.querySelector("span").textContent = newHead;
     backlog_content.querySelector("span").textContent = newAbout;
   }
+}
+
+edit_task.addEventListener("click", function (e) {
+  console.log(e);
+  // const select_id = edit_task.id;
+  sub_2.addEventListener("click", function () {
+    if (e.target.id === task1.id) {
+      forAddTask();
+    }
+  });
+  // console.log(select_id);
 });
 
-// edit_task.addEventListener("click", function () {
-//   const newHead = prompt("Enter new task heading:", backlog_head.innerText);
-//   // Prompt for new task details
-//   const newAbout = prompt(
-//     "Enter new task details:",
-//     backlog_content.textContent.trim()
-//   );
-
-//   // Update the task content if the user provided new values
-//   if (newHead !== null && newAbout !== null) {
-//     backlog_head.innerText = newHead;
-//     backlog_content.innerText = newAbout;
-//   }
-// });
-
 delete_task.addEventListener("click", function () {
+  const task = document.querySelector(".task");
+  // index = parseInt(div.dataset.index);
   task.remove();
 });
 
 const createLog = function () {
+  let newIndex = backlog.children.length + 1;
   const task_div = document.createElement("div");
   const head_div = document.createElement("div");
   const about_div = document.createElement("div");
   const delete_task = document.createElement("button");
   const edit_task = document.createElement("button");
+  total_task_count++;
+  current_task_count = newIndex;
+  // console.log(total_task_count);
+  task_div.dataset.index = newIndex;
+  task_div.id = total_task_count;
+  edit_task.id = total_task_count;
 
   const input_Head = document.getElementById("Backlog-Heading").value;
   const input_About = document.getElementById("backlog-Details").value;
@@ -135,14 +101,23 @@ const createLog = function () {
   about_div.classList.add("backlog_content");
 
   edit_task.textContent = "🖌️";
-  edit_task.classList.add("edit_task");
-  edit_task.classList.add("btn");
-  edit_task.classList.add("btn-primary");
+  edit_task.classList.add("edit_task", "btn", "btn-primary");
+  edit_task.setAttribute("data-bs-target", "#exampleModal-2");
+  edit_task.setAttribute("data-bs-toggle", "modal");
+
+  edit_task.addEventListener("click", function (e) {
+    sub_2.addEventListener("click", function () {
+      const newHead = document.querySelector(".my-backlog").value;
+      const newAbout = document.querySelector(".my-about").value;
+      if (newHead !== null && newAbout !== null) {
+        head.nodeValue = newHead;
+        about.nodeValue = newAbout;
+      }
+    });
+  });
 
   delete_task.textContent = "❌";
-  delete_task.classList.add("delete_task");
-  delete_task.classList.add("btn");
-  delete_task.classList.add("btn-secondary");
+  delete_task.classList.add("delete_task", "btn", "btn-secondary");
 
   task_div.appendChild(head_div);
   task_div.appendChild(about_div);
@@ -150,43 +125,32 @@ const createLog = function () {
   task_div.classList.add("task");
   task_div.setAttribute("draggable", "true");
 
-  backlog.appendChild(task_div);
+  // const allEdit = document.querySelectorAll(".task");
+  // console.log(allEdit);
 
-  task_div.addEventListener("dragstart", dragStart);
-  // task_div.addEventListener("dragend", dragEnd);
+  // allEdit.forEach(function (div) {
+  //   div.addEventListener("click", function () {
+  //     // Get the ID of the clicked element
+  //     let clickedDivId = this.getAttribute("id");
+  //   });
+  // });
 
-  console.log(task_div);
-  close();
-
-  edit_task.addEventListener("click", function () {
-    const newHead = prompt("Enter new task heading:", input_Head);
-    const newAbout = prompt("Enter new task details:", input_About);
-
-    // Update the task content if the user provided new values
-    if (newHead !== null && newAbout !== null) {
-      head.nodeValue = newHead;
-      about.nodeValue = newAbout;
-    }
-  });
+  showLogs(task_div);
 
   delete_task.addEventListener("click", function () {
+    const index = parseInt(task_div.dataset.index);
     task_div.remove();
   });
+  console.log(task_div);
 };
 
-submit.addEventListener("click", createLog);
+function showLogs(task) {
+  backlog.appendChild(task);
+  task.addEventListener("dragstart", dragStart);
+}
 
-////////////////////////////////////////////////////////////////////////////////////////////////
+submit.addEventListener("click", function () {
+  createLog();
+});
 
-// for add button:-------------------------------
-
-// backlog.appendChild(task_div);
-
-// task_div.addEventListener("dragstart", dragStart);
-// task_div.addEventListener("dragend", dragEnd);
-
-// // const html = `<div class="task" draggable=true>content${1}</div>`;
-// // backlog.insertAdjacentHTML("beforeend", html);
-// console.log(task_div);
-// const delete_button = document.querySelector(".delete_task");
-// delete_button.textContent = "x";
+//////////////////////////////////////////////////////////////////////////////////////////////
